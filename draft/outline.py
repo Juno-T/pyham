e0775d501fa137e9cbb814ee8b1e36fda8995394
Juno-T <yossathorn.tian@gmail.com>
2022-07-10 08:26:31 +0000

    add documentation, clean up

--- draft/outline.py
+++ draft/outline.py
@@ -2,8 +2,8 @@ from __future__ import annotations # for forward reference of HAM and type hint
 import copy
 from typing import Tuple, NamedTuple, Type, Any, Callable, Union
 
-# TODO*** Machine's customizable parameters. Urgent, for repetative actions.
 # TODO refactor
+# TODO What if choice machines have different kind of choice returned?
 # TODO HAMQ-INT internal transition skip?
 
 class JointState(NamedTuple):
@@ -27,6 +27,7 @@ class HAM:
     verbose: bool = False
   ):
     """
+      A hierarchical of abstract machine (HAM) class.
       Parameters:
         action_executor: A function that handle action execution. Must return (obsv, reward, done, info). Can be as simple as gym's env.step.
         transition_handler: A function that handle transition upon new transition is created (when there is a choice point).
@@ -38,12 +39,19 @@ class HAM:
     self.machine_count=0
     self.verbose =verbose
 
-  def set_observation(self, obsv):
-    self._current_observation=obsv
+  def set_observation(self, current_observation):
+    """
+      Set current observation.
+      Parameters:
+        current_observation: An observation to be set.
+    """
+    self._current_observation=current_observation
 
   def episodic_reset(self, current_observation):
     """
-      Must be called before each episode
+      Reseting internal values. Must be called before each episode.
+      Parameters:
+        current_observation: Initial observation of this episode.
     """
     self._current_observation = current_observation
     self._machine_stack = []
@@ -52,8 +60,13 @@ class HAM:
     self._previous_choice_point = None
     self._tmp_return = None
     
-
-  def choice_point_handler(self, choice, done=False):
+  def _choice_point_handler(self, choice, done=False):
+    """
+      Record choices and create transitions when possible.
+      Parameters:
+        choice: A choice, return of the learnable_choice_machine.
+        done: Whether this handler is being called on the episode termination or not.
+    """
     joint_state = JointState(
       s=self._current_observation,
       m=copy.deepcopy(self._machine_stack)
@@ -78,6 +91,10 @@ class HAM:
   def _add_machine(self, name, machine, representation=None):
     """
       Registering machine created by self._create_*_machine
+      Parameters:
+        name: Machine name to be registered
+        machine: A machine created by self._create_*_machine
+        representation: A representation of this machine. Used when pushing to machine stack. A unique integer is used as a default value.
     """
     if self.verbose:
       print(name+": \r")
@@ -92,7 +109,10 @@ class HAM:
 
   def functional_machine(self, func: Callable[[Type[HAM], Any], Any], representation=None):
     """
-      A convinent decorator for creating and registering functional machine
+      A convinent decorator for creating and registering functional machine.
+      Parameters:
+        func: A python function that takes exactly two arguments, HAM and an arbitrary argument.
+        representation: A representation of this machine. Used when pushing to machine stack. A unique integer is used as a default value.
     """
     machine_name = func.__name__
     machine = self._create_functional_machine(func)
@@ -102,6 +122,9 @@ class HAM:
   def action_machine(self, action_selector: Callable[[Type[HAM], Any], Any], representation=None):
     """
       A convinent decorator for creating and registering action machine
+      Parameters:
+        action_selector: A python function that takes exactly two arguments, HAM and an arbitrary argument. The return must be an action callable by `action_executor`.
+        representation: A representation of this machine. Used when pushing to machine stack. A unique integer is used as a default value.
     """
     machine_name = action_selector.__name__
     machine = self._create_action_machine(action_selector)
@@ -111,6 +134,9 @@ class HAM:
   def learnable_choice_machine(self, selector: Callable[[Type[HAM], Any], Any], representation=None):
     """
       A convinent decorator for creating and registering learnable choice machine
+      Parameters:
+        selector: A python function that takes exactly two arguments, HAM and an arbitrary argument. The return must be a choice, in any form.
+        representation: A representation of this machine. Used when pushing to machine stack. A unique integer is used as a default value.
     """
     machine_name = selector.__name__
     machine = self._create_learnable_choice_machine(selector)
@@ -120,45 +146,46 @@ class HAM:
   def _create_functional_machine(self, func: Callable[[Type[HAM], Any], Any]):
     """
       Parameters:
-        func: A pure python function
+        func: A python function that takes exactly two arguments, HAM and an arbitrary argument.
     """
     if self.verbose:
       print("Functional machine initiated")
     while True:
-      args = yield # args should be unified
+      args = yield
       ret = func(self, args)
       self._tmp_return = ret
 
   def _create_action_machine(self, action_selector: Callable[[Type[HAM], Any], Any]):
     """
       Parameters:
-        action_selector: A pure function that return an action given the input
+        action_selector: A python function that takes exactly two arguments, HAM and an arbitrary argument. The return must be an action callable by `action_executor`.
     """
     if self.verbose:
       print("Action machine initiated")
     while True:
-      args = yield # args should be unified
+      args = yield
       action = action_selector(self,args)
       self.CALL_action(action)
 
   def _create_learnable_choice_machine(self, selector: Callable[[Type[HAM], Any], Any]):
     """
       Parameters:
-        selector: A pure function that return an option given the input
+        selector: A python function that takes exactly two arguments, HAM and an arbitrary argument. The return must be a choice, in any form.
     """
     if self.verbose:
       print("Learnable Choice machine initiated")
     while True:
       args = yield
       choice = selector(self, args)
-      self.choice_point_handler(choice)
+      self._choice_point_handler(choice)
       self._tmp_return = choice
 
   def CALL(self, machine: Union[str, Callable], args=None):
     """
-      A method to CALL other registered machine.
+      A method to CALL a registered machine. This must be used instead of normal python's function calling.
       Parameters:
         machine: Machine's original function or it's name.
+        args: An argument to pass into the calling machine. Use compositional data types, list or dictionary, to pass more data.
     """
     if isinstance(machine, str):
       machine_name = machine
@@ -167,7 +194,7 @@ class HAM:
     assert(machine_name in self.machines)
     try:
       self._machine_stack.append(self.machines[machine_name]["representation"])
-      self.machines[machine_name]["machine"].send(args) # TODO this will define all the args
+      self.machines[machine_name]["machine"].send(args)
       self._machine_stack.pop()
     except StopIteration:
       print("STOP ITERATION??")
@@ -175,12 +202,17 @@ class HAM:
     return self._tmp_return
 
   def CALL_action(self, action):
+    """
+      A convenient method to CALL a trivial action.
+      Parameters:
+        action: An action to be executed with `action_executor`
+    """
     obsv, reward, done, info = self.action_executor(action)
     self.set_observation(obsv)
     self._cumulative_reward+=reward*self._cumulative_discount
     self._cumulative_discount*=self.reward_discount
     if done:
-      self.choice_point_handler(None, done = True)
+      self._choice_point_handler(None, done = True)
     self._tmp_return = obsv
 
 
