bd281d9ca1f8f3cc1093a015a83514fdbff86775
Juno-T <yossathorn.tian@gmail.com>
2022-07-15 02:25:03 +0000

    add error handling, add machine args handling

--- ham/ham.py
+++ ham/ham.py
@@ -26,7 +26,6 @@ class AlternateLock():
         self.cond.notifyAll()
         self.cond.release()
 
-
 class HAM:
 
   def __init__(
@@ -94,27 +93,8 @@ class HAM:
     self._tau=0
     return joint_state, reward, done, {}
 
-  # def _add_machine(self, name, machine, representation=None):
-  #   """
-  #     Registering machine created by self._create_*_machine
-  #     Parameters:
-  #       name: Machine name to be registered
-  #       machine: A machine created by self._create_*_machine
-  #       representation: A representation of this machine. Used when pushing to machine stack. A unique integer is used as a default value.
-  #   """
-  #   if self.verbose:
-  #     print(name+": \r")
-  #   next(machine)
-  #   if representation is None:
-  #     representation = self.machine_count
-  #   self.machine_count+=1
-  #   self.machines[name]={
-  #     "machine": machine,
-  #     "representation": representation,
-  #   }
-
   def machine(self, representation=None):
-    def register_func(func, representation=representation):
+    def register_func(func: Callable[[Type(HAM),],Any], representation=representation):
       if representation is None:
         representation = self.machine_count
       self.machine_count+=1
@@ -126,81 +106,12 @@ class HAM:
       return func
     return register_func
 
-  # def functional_machine(self, func: Callable[[Type[HAM], Any], Any], representation=None):
-  #   """
-  #     A convinent decorator for creating and registering functional machine.
-  #     Parameters:
-  #       func: A python function that takes exactly two arguments, HAM and an arbitrary argument.
-  #       representation: A representation of this machine. Used when pushing to machine stack. A unique integer is used as a default value.
-  #   """
-  #   machine_name = func.__name__
-  #   machine = self._create_functional_machine(func)
-  #   self._add_machine(machine_name, machine, representation)
-  #   return func
-
-  # def action_machine(self, action_selector: Callable[[Type[HAM], Any], Any], representation=None):
-  #   """
-  #     A convinent decorator for creating and registering action machine
-  #     Parameters:
-  #       action_selector: A python function that takes exactly two arguments, HAM and an arbitrary argument. The return must be an action callable by `action_executor`.
-  #       representation: A representation of this machine. Used when pushing to machine stack. A unique integer is used as a default value.
-  #   """
-  #   machine_name = action_selector.__name__
-  #   machine = self._create_action_machine(action_selector)
-  #   self._add_machine(machine_name, machine, representation)
-  #   return action_selector
-
-  # def choice_machine(self, machine_name, representation=None):
-  #   machine = self._create_choice_machine(machine_name)
-  #   self._add_machine(machine_name, machine, representation)
-  #   return machine_name
-
-  # def _create_functional_machine(self, func: Callable[[Type[HAM], Any], Any]):
-  #   """
-  #     Parameters:
-  #       func: A python function that takes exactly two arguments, HAM and an arbitrary argument.
-  #   """
-  #   if self.verbose:
-  #     print("Functional machine initiated")
-  #   while True:
-  #     args = yield
-  #     ret = func(self, args)
-  #     self._tmp_return = ret
-
-  # def _create_action_machine(self, action_selector: Callable[[Type[HAM], Any], Any]):
-  #   """
-  #     Parameters:
-  #       action_selector: A python function that takes exactly two arguments, HAM and an arbitrary argument. The return must be an action callable by `action_executor`.
-  #   """
-  #   if self.verbose:
-  #     print("Action machine initiated")
-  #   while True:
-  #     args = yield
-  #     action = action_selector(self,args)
-  #     self.CALL_action(action)
-
-  # def _create_choice_machine(self, machine_name):
-  #   """
-  #     Parameters:
-  #       selector: A python function that takes exactly two arguments, HAM and an arbitrary argument. The return must be a choice, in any form.
-  #   """
-  #   if self.verbose:
-  #     print("Choice machine initiated")
-  #   while True:
-  #     args = yield
-  #     self.current_choice_machine = machine_name
-  #     self.choice_point_lock.release_to("main")
-  #     self.choice_point_lock.acquire_for("ham")
-  #     # choice = selector(self, args)
-  #     # self._choice_point_handler(choice)
-  #     # self._tmp_return = choice
-
-  def CALL(self, machine: Union[str, Callable], args=None):
+  def CALL(self, machine: Union[str, Callable], args=[]):
     """
       A method to CALL a registered machine. This must be used instead of normal python's function calling.
       Parameters:
         machine: Machine's original function or it's function name.
-        args: An argument to pass into the calling machine. Use compositional data types, list or dictionary, to pass more data.
+        args: An argument to pass into the calling machine. Must be list or tuple.
     """
     if not self.is_alive :
       return 0
@@ -212,9 +123,11 @@ class HAM:
     self._machine_stack.append(self.machines[machine_name]["representation"])
     machine_return = None
     try:
-      machine_return = self.machines[machine_name]["func"](self, args)
+      machine_return = self.machines[machine_name]["func"](self, *args)
     except Exception as e:
+      self._machine_stack.pop()
       logging.error(f"{machine_name} machine failed: \n{traceback.format_exc()}\n{str(e)}")
+      return 0
     self._machine_stack.pop()
     return machine_return
 
@@ -231,7 +144,6 @@ class HAM:
     except Exception as e:
       logging.error(f"Error executing action: \n{traceback.format_exc()}\n{str(e)}")
       return 0
-    self._machine_stack.pop()
     self.set_observation(obsv)
     self._cumulative_reward+=reward*self._cumulative_discount
     self._cumulative_discount*=self.reward_discount
@@ -253,10 +165,11 @@ class HAM:
   def _start(self, machine, args):
       self.choice_point_lock.acquire_for("ham")
       ham_return = self.CALL(machine, args)
+      self.is_alive=False
       self.choice_point_lock.release_to("main")
       return ham_return
 
-  def start(self, machine: Union[str, Callable], args=None):
+  def start(self, machine: Union[str, Callable], args=[]):
     if self.is_alive:
       print("HAM is already running")
       return 0
