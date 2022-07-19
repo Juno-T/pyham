70055900c1985e2c840f33c05222c7af07548f47
juno-t <yossathorn.tian@gmail.com>
2022-07-19 02:57:23 +0000

    add env wrapper test, debug wrapper

--- ham/__init__.py
+++ ham/__init__.py
@@ -1,3 +1,3 @@
 from .ham import HAM
 from . import utils
-from .wrapper import WrappedEnv
+from .wrapper import WrappedEnv, create_concat_joint_state_wrapped_env
--- ham/wrapper.py
+++ ham/wrapper.py
@@ -1,5 +1,6 @@
 from typing import Optional, Union, Callable, Any
 import copy
+import numpy as np
 import gym
 from gym import spaces
 
@@ -31,6 +32,8 @@ class WrappedEnv(gym.Env):
     
     if self.will_render:
       self.ham.action_executor = self.create_wrapped_action_executor()
+    else:
+      self.ham.action_executor = self.env.step
     self.render_stack = []
 
   def create_wrapped_action_executor(self):
@@ -54,6 +57,7 @@ class WrappedEnv(gym.Env):
 
   def reset(self, seed:Optional[int]=None):
     cur_obsv = self.env.reset(seed=seed)
+    self.render_stack = []
     if self.will_render:
       rendered_frame = self.env.render(mode="rgb_array")
       self.render_stack.append(rendered_frame)
@@ -77,3 +81,69 @@ class WrappedEnv(gym.Env):
         self.ham.terminate()
       except:
         pass
+
+def create_concat_joint_state_wrapped_env(ham: HAM, 
+                                    env: gym.Env, 
+                                    choice_space: spaces.Space,
+                                    initial_machine: Union[Callable, str],
+                                    np_pad_config: dict,
+                                    machine_stack_cap: int = 1,
+                                    dtype=np.float32,
+                                    will_render=False
+                                    ):
+  """
+    Currently support only env with continuous observation space.
+
+    Parameters:
+      ham: HAM
+      env: gym.Env
+      choice_space: choice space designed in ham
+      np_pad_config: dictionary containing numpy.pad's mode and the reset of kwargs.
+        e.g. 
+        np_pad_config = {
+          "mode": "constant", # (default)
+          "constant_values": 0
+        }
+      machine_stack_cap: Number of top most machines to include in the joint state representation, pad if needed.
+    Return:
+      Wrapped env with joint state representation defined as concatenated numpy array between original env's observation space and fixed length of machine stack representations.
+  """
+
+  def _obj_len(obj):
+    if hasattr(obj, '__len__'):
+      return len(obj)
+    else:
+      return 1
+
+  # num_machines = ham.machine_count
+  repr_length = None
+  for machine_name, machine in ham.machines.items():
+    repr = machine['representation']
+    if repr_length is None:
+      repr_length = _obj_len(repr)
+    elif repr_length != _obj_len(repr):
+      raise("Unable to create joint state representation due to inconsistent representation length.")
+
+  machine_stack_repr_shape = (machine_stack_cap*repr_length,)
+  machine_stack_high = np.ones(machine_stack_repr_shape[0])
+  machine_stack_low = np.zeros(machine_stack_repr_shape[0])
+  og_obsv_high = env.observation_space.high
+  og_obsv_low = env.observation_space.low
+  choice_space = spaces.Discrete(env.action_space.n)
+  js_space = spaces.Box(np.hstack((og_obsv_low, machine_stack_low)),
+                                np.hstack((og_obsv_high, machine_stack_high)),
+                                dtype = dtype)
+  def js2obsv(js: JointState):
+    machine_stack_repr = np.hstack(js.m[-machine_stack_cap:])
+    padding = max(0, machine_stack_repr_shape[0]-len(machine_stack_repr))
+    machine_stack_repr = np.pad(machine_stack_repr, (padding,0), **np_pad_config)
+    js_repr = np.float32(np.hstack((js.s,machine_stack_repr)))
+    return js_repr
+  return WrappedEnv(ham, 
+                    env, 
+                    choice_space, 
+                    js_space, 
+                    js2obsv, 
+                    initial_machine=initial_machine,
+                    will_render=will_render)
+  
