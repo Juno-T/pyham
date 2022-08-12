ee9a69c6c962753a2c86bf7c0d849a6553b94dfe
juno-t <yossathorn.tian@gmail.com>
2022-08-12 07:18:39 +0000

    update choicepoint manager api

--- ham/choicepoint.py
+++ ham/choicepoint.py
@@ -2,7 +2,6 @@ from typing import List, NamedTuple
 from gym import spaces
 import numpy as np
 
-from pyham.ham import HAM
 
 class Choicepoint(NamedTuple):
   name: str
@@ -11,7 +10,7 @@ class Choicepoint(NamedTuple):
   id: int=-1
 
 class ChoicepointsManager:
-  def __init__(self, choicepoints: List[Choicepoint], eval=False):
+  def __init__(self, eval=False):
     """
     Parameters:
       choicepoints
@@ -21,15 +20,32 @@ class ChoicepointsManager:
       choicepoints_order
       N
     """
-    self.N = len(choicepoints)
-    self.choicepoints_order = [cp.name for cp in choicepoints]
-    self.choicepoints = {
-      cp.name: cp._replace(id=i)
-      for i, cp in enumerate(choicepoints)
-    }
+    self.N = 0
+    self.choicepoints_order = []
+    self.tau = []
+    self.choicepoints = {}
+    self.eval = eval
+
+  def set_eval(self, eval):
+    if eval:
+      self.init_discounts = np.ones((self.N,))
+    else:
+      self.init_discounts = np.array([
+        self.choicepoints[name].discount 
+        for name in self.choicepoints_order
+      ])
+
+  def add_choicepoint(self, choicepoint: Choicepoint):
+    """
+    """
+    assert(not choicepoint.name in self.choicepoints_order)
+    choicepoint = choicepoint._replace(id=self.N)
+    self.N+=1
+    self.choicepoints_order.append(choicepoint.name)
+    self.choicepoints[choicepoint.name] = choicepoint
 
     # Reward calculation setup
-    if eval:
+    if self.eval:
       self.init_discounts = np.ones((self.N,))
     else:
       self.init_discounts = np.array([
@@ -41,21 +57,25 @@ class ChoicepointsManager:
     """
       Episodic reset
     """
-    self.discounts = np.ones((self.N,))
     self.cumulative_rewards = np.zeros((self.N,))
+    self.discounts = np.ones((self.N,))
+    self.tau = np.zeros((self.N,))
 
   def distribute_reward(self, reward: float):
     rewards = self.discounts*reward
     self.cumulative_rewards+=rewards
     self.discounts*=self.init_discounts
+    self.tau+=1
 
   def reset_choicepoint(self, cp_name: str):
     cp_idx = self.choicepoints[cp_name].id
     
     cp_cumulative_reward = self.cumulative_rewards[cp_idx]
+    cp_tau = self.tau[cp_idx]
 
     self.cumulative_rewards[cp_idx]=0
     self.discounts[cp_idx]=1
+    self.tau[cp_idx]=0
 
-    return cp_cumulative_reward
+    return cp_cumulative_reward, cp_tau
   
